4e36be677a85346888336047fc8ed2fdb9c1a159
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-03-01 21:14:33 +0530

    Time: 12 ms (64.80%) | Memory: 42.2 MB (30.68%) - LeetSync

---
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+class Solution {
+public:
+    int countMatchingSubarrays(std::vector<int>& nums, std::vector<int>& pattern) {
+        int n = nums.size();
+        int m = pattern.size();
+        int count = 0;
+
+        for (int i = 0; i <= n - m; i++) {  // Adjusted loop condition
+            bool f = false;  // Moved inside the loop
+ if(i+m>=n)
+                break;
+            for (int k = 0; k < m; k++) {  // Adjusted loop condition
+                if (pattern[k] == 1) {
+                    if (nums[i + k + 1] > nums[i + k]) continue;
+                } else if (pattern[k] == 0) {
+                    if (nums[i + k + 1] == nums[i + k]) continue;
+                } else if (pattern[k] == -1) {
+                    if (nums[i + k + 1] < nums[i + k]) continue;
+                }
+
+                f = true;
+                break;
+            }
+
+            if (!f) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
