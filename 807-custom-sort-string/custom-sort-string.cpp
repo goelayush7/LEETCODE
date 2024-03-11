d983246bc6956518bbd1807d04536d33289615e2
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-03-11 20:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (55.71%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -3,16 +3,7 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        // Use 'vector' instead of 'int' arrays
-        vector<int> arr1(26, 0);
-        vector<int> arr2(128, 0); // Increase array size to cover ASCII characters
-
-        // Count occurrences of characters in 'order'
-        for (int i = 0; i < order.size(); i++) {
-            arr1[order[i] - 'a']++;
-        }
-
-        // Count occurrences of characters in 's'
+        vector<int> arr2(128, 0); 
         for (int i = 0; i < s.size(); i++) {
             arr2[s[i]]++;
         }
@@ -21,7 +12,6 @@ public:
         int j = 0;
         string ans = "";
 
-        // Process characters in 'order'
         while (i < order.size()) {
             if (arr2[order[i]] != 0) {
                 ans += order[i];
@@ -31,8 +21,7 @@ public:
             }
         }
 
-        // Process remaining characters in 's'
-        for (int i = 0; i < 128; i++) { // Iterate over all ASCII characters
+        for (int i = 0; i < 128; i++) { 
             while (arr2[i] > 0) {
                 ans += char(i);
                 arr2[i]--;
