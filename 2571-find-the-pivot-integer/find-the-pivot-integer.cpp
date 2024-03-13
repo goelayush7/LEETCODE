315e429b581d1082ba0ceeebddbf5d47c09ce2cb
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-03-13 19:40:05 +0530

    Time: 3 ms (51.39%) | Memory: 7.3 MB (15.68%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,24 +1,36 @@
+// class Solution {
+// public:
+//     int findsum(int i , int j ){
+//         int sum=0;
+//         for(int a = i ; a<=j ; a++) sum+=a;
+//         return sum;
+//     }
+//     int pivotInteger (int n) {
+//         if(n==1) return 1;
+//         int start = 0;
+//         int end = n;
+//         while(start<=end){
+//             int mid = (start+end)/2;
+//             if(findsum(0,mid)==findsum(mid,n)){
+//                 return mid;
+//             }
+//             if(findsum(0,mid)<findsum(mid,n)){
+//                 start = mid+1;
+//             }
+//             else end = mid-1;
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
-    int findsum(int i , int j ){
-        int sum=0;
-        for(int a = i ; a<=j ; a++) sum+=a;
-        return sum;
-    }
-    int pivotInteger (int n) {
-        if(n==1) return 1;
-        int start = 0;
-        int end = n;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(findsum(0,mid)==findsum(mid,n)){
-                return mid;
-            }
-            if(findsum(0,mid)<findsum(mid,n)){
-                start = mid+1;
-            }
-            else end = mid-1;
-        }
-        return -1;
+    int pivotInteger(int n) {
+        int sum = n * (n + 1) / 2;
+        double a = sqrt(sum);
+
+        if (a - ceil(a) == 0)
+            return int(a);
+        else
+            return -1;
     }
 };
