54486113c31a9aa2da6d9f2ae8af2f0861235bc7
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-03-17 12:41:57 +0530

    Time: 14 ms (38.68%) | Memory: 20.4 MB (58.08%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         intervals.push_back(newInterval);
+        int n = intervals.size(); // Size of the array 
+        vector<vector<int>>ans;
+
+        // sort the given intervals
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
-        int n = intervals.size();
-        for(int i =0;i<n ; i++ ){
-            int start = intervals[i][0];
-            int end  = intervals[i][1];
-            if(!ans.empty() && end <=ans.back()[1]){
-                continue;
+        for(int i=0;i<n;i++)
+        {
+            //if the current interval does not exist in the last interval
+            if(ans.empty() || intervals[i][0]> ans.back()[1])
+            {
+                ans.push_back(intervals[i]);
             }
-            
-            for(int j=i+1 ; j < n ; j++){
-                if(intervals[j][0]<=end){
-                    end= max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else
+            {
+                //if the current interval lies in the last interval
+                ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
     }
