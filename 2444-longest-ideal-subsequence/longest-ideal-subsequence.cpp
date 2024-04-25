9fafa28374bda56e0f949bbc3f6abe432f2ac4c4
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-04-25 17:58:24 +0530

    Time: 229 ms (46.64%) | Memory: 31.1 MB (43.70%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -4,19 +4,19 @@ private:
     int f(int idx,int prevChar,string &s,int k) {
         // base cases
         if(idx < 0) return 0;
-        if(dp[idx][prevChar] != -1) return dp[idx][prevChar];
+        if(dp[idx][prevChar+1] != -1) return dp[idx][prevChar+1];
 
         char ch = prevChar + 'a';
         int take = 0;
-        if(prevChar == 26 or abs(ch-s[idx]) <= k) take = 1 + f(idx-1,s[idx]-'a',s,k);
+        if(prevChar == -1 or abs(ch-s[idx]) <= k) take = 1 + f(idx-1,s[idx]-'a',s,k);
         int notTake = f(idx-1,prevChar,s,k);
 
-        return dp[idx][prevChar] = max(take,notTake);
+        return dp[idx][prevChar+1] = max(take,notTake);
     }
 public:
     int longestIdealString(string s, int k) {
         memset(dp,-1,sizeof(dp));
         int n = s.length();
-        return f(n-1,26,s,k);
+        return f(n-1,-1,s,k);
     }
 };
