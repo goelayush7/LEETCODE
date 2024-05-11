878411f657ae1704b44c37532da4664853854e37
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-05-11 19:07:21 +0530

    Time: 10 ms (93.36%) | Memory: 16.9 MB (59.05%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    class compare{ //comaprator for max-heap priority queue
+      public:
+        bool operator()(ListNode* first, ListNode* second){
+            return first->val > second->val;
+        }
+    };
+    
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // could be solved using a min - priority queue of 
+        // insert first node of each list, create the final list
+        // one at a time from the nodes popped from the heap
+        
+        priority_queue<ListNode*, vector<ListNode*>, compare>heap;
+        for(int i=0; i<lists.size();i++){
+         if(lists[i])
+            heap.push(lists[i]);
+        }
+        ListNode *dummy= new ListNode(0);
+        ListNode *head = dummy;
+        while(!heap.empty()){
+            head->next = heap.top();
+            heap.pop();
+            if(head->next && head->next->next) //do not push null in the heap, compare will fail
+                heap.push(head->next->next);
+            head = head->next;
+                
+        } 
+        return dummy->next;
+    }
+};
