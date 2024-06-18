2c5ce812c5bf2e9c25ec8eb0d538ad95e8e9c63f
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-06-18 19:17:15 +0530

    Time: 823 ms (11.34%) | Memory: 41.2 MB (47.29%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,35 +1,23 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
+                            vector<int>& worker) {
         int maxprofit = 0;
-        vector<pair<int, int>> diffprof;
-        
-        // Combine difficulty and profit into a vector of pairs
-        for (int i = 0; i < profit.size(); ++i) {
-            diffprof.push_back({difficulty[i], profit[i]});
+        vector<pair<int,int>>diff;
+        for (int i = 0; i < profit.size(); i++) {
+            diff.push_back({difficulty[i], profit[i]});
         }
-        
-        // Sort the diffprof by difficulty, then by profit
-        sort(diffprof.begin(), diffprof.end());
-        
-        // Sort workers to ease the process of finding the best job they can do
-        sort(worker.begin(), worker.end());
-        
-        int j = 0, best = 0;
-        
-        // Traverse each worker to find the maximum profit they can get
-        for (int i = 0; i < worker.size(); ++i) {
-            while (j < diffprof.size() && worker[i] >= diffprof[j].first) {
-                best = max(best, diffprof[j].second);
-                ++j;
+        int best = 0;
+        sort(diff.begin(),diff.end());
+        sort(worker.begin(),worker.end());
+        for (int i = 0; i < worker.size(); i++) {
+            int j = 0;
+            while (j<profit.size() && worker[i]>=diff[j].first){
+                best = max(best,diff[j].second);
+                j++;
             }
-            maxprofit += best;
+            maxprofit+=best;
         }
-        
         return maxprofit;
     }
 };
