3e4f9ae0776921675c7ab1dc7ee33c3600711502
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-06-21 17:58:24 +0530

    Time: 13 ms (97.87%) | Memory: 34.3 MB (82.09%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int k) {
         int n = grumpy.size();
-        int wpo = 0; // Initialize window satisfied customers
-
-        // Pre-calculate satisfied customers when not grumpy
-        for (int i = 0; i < n; i++) {
-            if (grumpy[i] == 0) {
+        int wpo = 0;
+        for(int i =0;i<n;i++){
+            if(grumpy[i]==0){
                 wpo += customers[i];
             }
         }
-
-        int i = 0, j = 0;
-        int maxi = 0; // Maximum satisfied customers in a window
-
-        // Slide the window and update wpo
-        while (j < k) {
-            wpo += grumpy[j] * customers[j]; // Add customers only if grumpy in window
+        int i =0;
+        int j =0;
+        int maxi =0;
+        while(j<k){
+            if(grumpy[j]==1){
+                wpo+=customers[j];
+            }
             j++;
-            maxi = max(maxi, wpo);
+            maxi= max(maxi,wpo);
         }
-
-        while (i < n && j < n) {
-            wpo -= grumpy[i] * customers[i]; // Remove customers if grumpy leaving window
+        while(i<n && j<n){
+            if(grumpy[i]==1){
+                wpo-=customers[i];
+            }
             i++;
-            wpo += grumpy[j] * customers[j]; // Add customers if grumpy entering window
-            maxi = max(maxi, wpo);
+            if(grumpy[j]==1){
+                wpo+=customers[j];
+            }
+            maxi = max(maxi,wpo);
             j++;
         }
-
         return maxi;
     }
 };
