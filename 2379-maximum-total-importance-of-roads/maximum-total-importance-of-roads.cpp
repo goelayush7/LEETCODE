d75c1446a1738fd735f0b818f8478c08575785e6
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-06-28 23:28:04 +0530

    Time: 420 ms (23.62%) | Memory: 153.7 MB (29.31%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-static bool cmp(pair<int,int> &a,pair<int,int>&b)
+    static bool cmp(pair<int,int> &a,pair<int,int>&b)
     {
         return a.first>b.first;
     }
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        long long ans = 0;
-        map<int, int> degree;
-        for (auto road : roads) {
-            degree[road[0]]++;
-            degree[road[1]]++;
+        long long ans=0;
+        map<int,int>maps;
+       for (auto road : roads) {
+            maps[road[0]]++;
+            maps[road[1]]++;
         }
-        vector<pair<int, int>> degreeNodePairs;
-        for (auto it : degree) {
-            degreeNodePairs.push_back({it.second, it.first});
-        }
-        sort(degreeNodePairs.begin(), degreeNodePairs.end(),cmp);
-        vector<int> nodeValues(n, 0);
-        int value = n;
-        for (auto pair : degreeNodePairs) {
-            nodeValues[pair.second] = value--;
-        }
-        for (auto road : roads) {
-            ans += nodeValues[road[0]] + nodeValues[road[1]];
-        }
-        return ans;
+
+       vector<pair<int,int>>cpy;
+       for(auto it : maps){
+        cpy.push_back({it.second,it.first});
+       }
+       sort(cpy.begin(),cpy.end(),cmp);
+       vector<int>val(n,-1);
+       int maxi  =n;
+       for(int i =0;i<cpy.size();i++){
+            int index = cpy[i].second;
+            val[index]=maxi;
+            maxi--;
+       }
+       for(int i =0;i<roads.size();i++){
+            int index1 = roads[i][0];
+            int index2 = roads[i][1];
+            ans+=val[index1]+val[index2];
+       }
+       return ans;
     }
 };
