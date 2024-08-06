44522dcd91e56a50b9cf727a6515702c37336731
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-08-06 19:57:01 +0530

    Time: 13 ms (84.21%) | Memory: 26.7 MB (68.79%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ele =1;
-        int n = nums.size();
-        int count=0;
-        int i=0;
-        while(i<n){
+        int count =0;
+        int n =nums.size();
+        int ele = 1;
+        for(int i =0;i<n;i++){
             if(count==0){
-                ele=nums[i];
+                ele = nums[i];
                 count=1;
             }
-            else if(ele==nums[i]){
-             
-                count++;
-            }
-            else{
+            else if(ele != nums[i]){
                 count--;
             }
-            i++;
+            else count++;
         }
-        //ele can be our element
         count =0;
         for(int i =0;i<n;i++){
-            if(ele==nums[i]){
-                count++;
-            }
+            if(nums[i]==ele)count++;
         }
         if(count>n/2)return ele;
         return -1;
-        
     }
 };
