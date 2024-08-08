2d24b491963a19746322bd90fc79ec707a604fa7
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-08-08 18:23:21 +0530

    Time: 8 ms (45.40%) | Memory: 12.6 MB (63.30%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void helper(vector<int>& candidates, int target,int n,vector<int>&out,vector<vector<int>>&ans,int idx){
+    void fun(vector<int>& candidates, int target, int i, vector<int>& out,
+             vector<vector<int>>& ans, int n) {
         if(target==0){
-            sort(ans.begin(),ans.end());
             ans.push_back(out);
             return;
         }
-        if(target<0 || idx<0){
+        if(target<0 || i==n){
             return;
         }
-        //exclude;
-        helper(candidates,target,n,out,ans,idx-1);
-        //include;
-        out.push_back(candidates[idx]);
-        helper(candidates,target-candidates[idx],n,out,ans,idx);
-        out.pop_back();
+        if(candidates[i]<=target){
+            out.push_back(candidates[i]);
+            fun(candidates,target-candidates[i],i,out,ans,n);
+            out.pop_back();
+        }
+        fun(candidates,target,i+1,out,ans,n);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>out;
-        vector<vector<int>>ans;
-        helper(candidates,target,candidates.size(),out,ans,candidates.size()-1);
+        vector<int> out;
+        vector<vector<int>> ans;
+        fun(candidates,target,0,out,ans,candidates.size());
         return ans;
     }
 };
