853fc9b27b8b859a3ae027128877b9ace10a3719
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-11-06 19:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (63.03%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int checksetbits(int n) {
         int count = 0;
-        for (int i = 0; i < 32; i++) { // Assuming 32-bit integer
+        for (int i = 0; i < 32; i++) {
             if (n & (1 << i)) {
                 count++;
             }
         }
         return count;
     }
-    bool checksort(vector<int>nums){
-        for(int i =1;i<nums.size();i++){
-            if(nums[i-1]>nums[i])return false;
-        }
-        return true;
-    }
     bool canSortArray(vector<int>& nums) {
-        int n = nums.size();
-        for(int i =0;i<nums.size();i++){
-            for(int j =0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1] && checksetbits(nums[j])==checksetbits(nums[j+1])){
-                    swap(nums[j],nums[j+1]);
-                }
+        int prevmax = INT_MIN;
+        int curmin = nums[0];
+        int curmax =nums[0];
+        int setBits = checksetbits(nums[0]);
+        for(int i =1;i<nums.size();i++){
+            if(checksetbits(nums[i])==setBits){
+                curmax = max(curmax,nums[i]);
+                curmin = min(curmin,nums[i]);
+            }
+            else{
+                if(prevmax>curmin)return false;
+                prevmax = curmax;
+                setBits = checksetbits(nums[i]);
+                curmin = nums[i];
+                curmax = nums[i];
             }
         }
-        if(checksort(nums))return true;
-        return false;
+        return curmin>prevmax;
     }
 };
