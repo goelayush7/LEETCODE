09c73cc88207503d0b5d49905a0b14e22432730f
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-01-06 18:33:53 +0530

    Time: 33 ms (16.66%) | Memory: 70.5 MB (58.54%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    bool helper(int mid, vector<int>& bloomDay, int m, int k) {
-        int cons = 0;
-        int count = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
-            if (bloomDay[i] <= mid) {
-                cons++;
-                if (cons == k) {
-                    count++;
-                    cons = 0;
+    bool trial(vector<int>&bloomday,int m,int k ,int mid){
+        int flowers = 0;
+        int boq = 0;
+        for(int i =0;i<bloomday.size();i++){
+            if(bloomday[i]<=mid){
+                flowers++;
+                if(flowers==k){
+                    boq++;
+                    flowers=0;
                 }
-            } else {
-                cons = 0;
             }
+            else flowers=0;
         }
-        return count >= m;
+        return boq>=m;
     }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-      int n = bloomDay.size();
-        if ((long long)m * k > n) return -1;
-
-
-        int start = *min_element(bloomDay.begin(), bloomDay.end());
-        int end = *max_element(bloomDay.begin(), bloomDay.end());
-        int mini = INT_MAX;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (helper(mid, bloomDay, m, k)) {
-                mini = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+        int start =1;
+        int end = *max_element(bloomDay.begin(),bloomDay.end());
+        int ans = -1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            bool flag = trial(bloomDay,m,k,mid);
+            if(flag){
+                ans = mid;
+                end = mid-1;
             }
+            else start = mid+1;
         }
-        return mini;
+        return ans;
     }
 };
