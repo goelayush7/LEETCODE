5f7ff8e1fe991597fa77027c3f7aabbad39b1496
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-01-06 17:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (67.01%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int>ans(boxes.size(),0);
-        for(int i =0;i<boxes.size();i++){
-            for(int j = i+1;j<boxes.size();j++){
-                if(boxes[j]=='1'){
-                    ans[i]+=abs(j-i);
-                }
+        int n = boxes.size();
+        vector<int> ans(n, 0);
+
+        // First pass (left-to-right)
+        int count1 = 0; // Number of balls seen so far
+        int sum = 0;    // Sum of operations for the current index
+        for (int i = 0; i < n; i++) {
+            ans[i] = sum;
+            if (boxes[i] == '1') {
+                count1++;
             }
+            sum += count1;
         }
-        for(int i =boxes.size()-1;i>=0;i--){
-            for(int j = i-1;j>=0;j--){
-                if(boxes[j]=='1'){
-                    ans[i]+=abs(j-i);
-                }
+
+        // Second pass (right-to-left)
+        count1 = 0;
+        sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] += sum;
+            if (boxes[i] == '1') {
+                count1++;
             }
+            sum += count1;
         }
+
         return ans;
     }
 };
