d1f414a0d917c224b1efb88b51ff0939752073da
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-02-11 22:57:51 +0530

    Time: 51 ms (15.36%) | Memory: 32.2 MB (54.61%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-   bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
-        int next = hand[i] + 1;
-        hand[i] = -1;  // Mark as used
-        int count = 1;
-        i += 1;
-        while (i < n && count < groupSize) {
-            if (hand[i] == next) {
-                next = hand[i] + 1;
-                hand[i] = -1;
-                count++;
-            }
-            i++;
-        }
-        return count == groupSize;
-    }
+    bool isNStraightHand(vector<int>& hand, int gs) {
+        if (hand.size() % gs != 0)
+            return false;
 
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if (n % groupSize != 0) return false;
-        std::sort(hand.begin(), hand.end());
-        for (int i = 0; i < n; i++) {
-            if (hand[i] >= 0) {
-                if (!findSuccessors(hand, groupSize, i, n)) return false;
+        map<int, int> freq; // Use ordered map to maintain sorted keys
+        for (int num : hand) {
+            freq[num]++;
+        }
+
+        while (!freq.empty()) {
+            int start = freq.begin()->first; // Smallest number in hand
+            for (int i = 0; i < gs; i++) {
+                if (freq[start + i] == 0) 
+                    return false; // If we can't form a group
+                
+                freq[start + i]--; // Use one instance of start + i
+                
+                if (freq[start + i] == 0) 
+                    freq.erase(start + i); // Remove when count reaches 0
             }
         }
         return true;
