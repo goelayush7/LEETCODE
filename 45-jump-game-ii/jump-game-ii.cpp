3ab3462fbf3a5a1813fdd959a3cec187a6e9b0e6
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-02-14 19:16:49 +0530

    Time: 219 ms (6.62%) | Memory: 22.6 MB (6.64%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -4,21 +4,22 @@ public:
         if (index == nums.size() - 1) {
             return 0;
         }
+        if (index >= nums.size()) {
+            return INT_MAX;
+        }
         if (dp[index] != -1) {
             return dp[index];
         }
         
         long long int count = INT_MAX;
-        for (int i = 1; i <= nums[index]; i++) { 
-            if (index + i < nums.size()) {
-                count = min(count, 1 + helper(nums, index + i, dp));
-            }
+        for (int i = 1; i <= nums[index]; i++) {
+            count = min(count, 1 + helper(nums, index + i, dp));
         }
         return dp[index] = count;
     }
 
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);  // Initialize DP array with -1
+        vector<int> dp(nums.size(), -1); // Initialize DP array with -1
         return helper(nums, 0, dp);
     }
 };
