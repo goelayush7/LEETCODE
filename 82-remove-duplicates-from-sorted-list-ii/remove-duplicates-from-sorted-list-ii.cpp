13cb0d399ca46687ad29b49fa318cdcf8046bf5c
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-04-10 18:37:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (40.04%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,38 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        unordered_map<int,int>maps;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            int value = temp->val;
-            maps[value]++;
-            temp = temp->next;
-        }
-        ListNode* ans = new ListNode(0);
-        vector<int>t;
-        ListNode* cpy =ans;
-        for(auto it : maps){
-            if(it.second==1){
-                t.push_back(it.first);
-            }
-        }
-        sort(t.begin(),t.end());
-        for(auto it : t){
-            ListNode* node = new ListNode(it);
-            ans->next = node;
-            ans = ans->next;
-        }
-        return cpy->next;
+        if (!head || !head->next) return head;
 
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* prev = &dummy;
+        ListNode* cur = head;
+
+        while (cur && cur->next) {
+            if (cur->val == cur->next->val) {
+                // Skip all duplicates
+                while (cur->next && cur->val == cur->next->val) {
+                    cur = cur->next;
+                }
+                // Remove all of them
+                prev->next = cur->next;
+            } else {
+                prev = cur;
+            }
+            cur = cur->next;
+        }
+
+        return dummy.next;
     }
 };
