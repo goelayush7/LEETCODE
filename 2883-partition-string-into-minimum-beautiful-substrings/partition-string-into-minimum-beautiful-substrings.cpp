8161bb510a95b34468bf42640a95e3b82ba1d2c0
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-04-12 20:14:28 +0530

    Time: 16 ms (8.71%) | Memory: 15.7 MB (5.39%) - LeetSync

--- 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int helper(string s,int i,unordered_set<string>st){
+    int helper(string s,int i,unordered_set<string>st,vector<int>&dp){
         if(i==s.size()){
             return 0;
         }
+        if(dp[i]!=-1)return dp[i];
         int ans = 100000;
         for(int j = i;j<s.size();j++){
             if(st.find(s.substr(i,j-i+1))!=st.end()){
-                ans = min(ans,1+helper(s,j+1,st));
+                ans = min(ans,1+helper(s,j+1,st,dp));
             }
             else continue;
         }
-        return ans;
+        return dp[i] = ans;
     }
     int minimumBeautifulSubstrings(string s) {
         unordered_set<string> st{
             "11110100001001", "110000110101", "1001110001", "1111101",
             "11001",          "101",          "1"};
-        int ans = helper(s,0,st);
+        vector<int>dp(s.size(),-1);
+
+        int ans = helper(s,0,st,dp);
         if(ans==100000)return -1;
         return ans;
         
