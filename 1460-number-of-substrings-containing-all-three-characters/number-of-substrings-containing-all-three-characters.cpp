e5ef14bb1db2c10c8e693f809888ed6287653e8b
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-04-15 20:44:19 +0530

    Time: 39 ms (14.79%) | Memory: 17 MB (6.74%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int len = s.length();
-        int left = 0, right = 0;
-        // Track frequency of a, b, c
-        vector<int> freq(3, 0);
-        int total = 0;
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int n = s.size();
+        unordered_map<char, int> maps;
 
-        while (right < len) {
-            // Add character at right pointer to frequency array
-            char curr = s[right];
-            freq[curr - 'a']++;
-
-            while (hasAllChars(freq)) {
-                total += len - right;
-
-                char leftChar = s[left];
-                freq[leftChar - 'a']--;
-                left++;
+        while (j < s.size()) {
+            maps[s[j]]++; 
+            while (maps.size() == 3) {
+                count += n - j;  
+                maps[s[i]]--;  
+                if (maps[s[i]] == 0) {
+                    maps.erase(s[i]);
+                }
+                i++;
             }
-
-            right++;
+            j++;
         }
-
-        return total;
-    }
-
-private:
-    bool hasAllChars(vector<int>& freq) {
-        return freq[0] > 0 && freq[1] > 0 && freq[2] > 0;
+        return count;
     }
 };
