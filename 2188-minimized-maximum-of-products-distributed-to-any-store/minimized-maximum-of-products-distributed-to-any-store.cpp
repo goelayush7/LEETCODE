ff77936fdb5fcee5cfe72e42b5fabfa513db63d5
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-04-18 19:11:07 +0530

    Time: 23 ms (88.10%) | Memory: 88.5 MB (17.00%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    bool func(int n,vector<int>&q,int mid){
-        int maxi = 0;
-        for(auto it : q){
-            int curcount  = (it+mid-1)/mid;
-            maxi +=curcount;
-            if(maxi>n)return false;
+    bool helper(int n,vector<int>&quantities,int maxi){
+        int count  = 0;
+        for(int i = 0;i<quantities.size();i++){
+            count += (quantities[i] + maxi - 1) / maxi;
         }
-        return true;
+        return count<=n;
     }
-    int minimizedMaximum(int n, vector<int>& q) {
+    int minimizedMaximum(int n, vector<int>& quantities) {
         int start =1;
-        int end = *max_element(q.begin(),q.end());
-        int ans =0;
+        int end = *max_element(quantities.begin(),quantities.end());
         while(start<=end){
-            int mid = start+(end-start)/2;
-            if(func(n,q,mid)){
-                ans = mid;
+            int mid = start + (end-start)/2;
+            if(helper(n,quantities,mid)){
                 end = mid-1;
             }
-            else{
-                start = mid+1;
-            }
+            else start = mid+1;
         }
-        return ans;
+        return end+1;
     }
 };
