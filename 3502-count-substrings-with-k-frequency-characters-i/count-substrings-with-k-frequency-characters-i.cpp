7377f18cf475ce38bbdf0c8a51a486fecfa617f1
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-05-09 19:34:56 +0530

    Time: 4 ms (47.99%) | Memory: 10.4 MB (46.13%) - LeetSync

--- 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.cpp
+++ 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int numberOfSubstrings(string s, int k) {
-        int i = 0, j = 0; // Two pointers for sliding window
-        int count = 0;    // To count the valid substrings
-        int n = s.size(); // Size of the string
-        unordered_map<char, int> maps; // Map to store frequency of characters
-
-        while (i < n) {
-            // Increment the frequency of the character at position i
+        int j = 0;
+        int ans = 0;
+        int n = s.size();
+        unordered_map<int,int>maps;
+        for(int i = 0;i<n;i++){
             maps[s[i]]++;
-
-            // If the frequency of the current character is greater than or equal to k
-            while (maps[s[i]] >= k) {
-                // Every substring starting from `i` to the end is valid
-                count += n - i;
-
-                // Now try to shrink the window from the left (increment `j`)
+            while(maps[s[i]]>=k){
+                ans+=n-i;
                 maps[s[j]]--;
-                j++; // Move `j` to shrink the window
+                j++;
             }
-
-            i++;
-        }
-
-        return count;
+        }   
+        return ans;
     }
 };
