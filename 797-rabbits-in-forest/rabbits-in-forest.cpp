41476bc10631ad0a9de1fcc8e5a46aaf10c585ad
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-05-16 19:08:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (44.85%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
-    int numRabbits(vector<int>& nums, int total = 0) {
-        unordered_map <int,int> mpp;
-        for (int i : nums) mpp[i]++;
-
-        for (auto& p : mpp)
-        total += ceil((double)p.second / (p.first + 1)) * (p.first + 1);
-        return total;
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> maps;
+        int ans = 0;
+        int countzero =0;
+        for (auto it : answers) {
+            if(it==0){
+                countzero++;
+                continue;
+            }
+            maps[it]++;
+        }
+        for (auto it : maps) {
+            double value = it.first+1;
+            double count = it.second;
+            if(count>value){
+                ans+=ceil(count/(value))*value;
+            }
+            else ans+=value;   
+        }
+        cout<<countzero;
+        return ans+=countzero;
     }
 };
