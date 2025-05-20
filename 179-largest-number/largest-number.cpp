b738364bfeeffce35841ebfe619eca521d92deb4
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-05-20 19:05:53 +0530

    Time: 7 ms (27.57%) | Memory: 16.9 MB (66.40%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,22 +1,15 @@
-
-bool compare(string a,string b){
-    return a+b > b+a;
-}
-
 class Solution {
 public:
-    string largestNumber(vector<int>& nums) {
-	
-        vector<string> container;
-		
-        for(int i : nums)  container.push_back(to_string(i));
-        
-        sort(container.begin(),container.end(),compare);
-        
-        string result;
-        
-        for(int i=0;i<container.size();i++)  result+=container[i];
-        
-        return result[0]=='0'? "0" : result;
+    string largestNumber(vector<int> &num) {
+        vector<string> arr;
+        for(auto i:num)
+            arr.push_back(to_string(i));
+        sort(begin(arr), end(arr), [](string &s1, string &s2){ return s1+s2>s2+s1; });
+        string res;
+        for(auto s:arr)
+            res+=s;
+        while(res[0]=='0' && res.length()>1)
+            res.erase(0,1);
+        return  res;
     }
 };
