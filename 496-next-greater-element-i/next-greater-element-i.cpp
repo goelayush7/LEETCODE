64b7f3932c8855e50277a5757461e97c217df51d
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-05-24 13:51:56 +0530

    Time: 3 ms (42.84%) | Memory: 12.7 MB (73.46%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp;
-        for(int i =0;i<nums2.size();i++){
-            mp[nums2[i]] = i;
-        }
-        vector<int>temp(nums2.size(),0);
-        stack<int>st;
+        stack<int> st;
         int n = nums2.size();
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty() && st.top()<nums2[i]){
+        unordered_map<int, int> maps;
+        vector<int> temp(n,-1);
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
             }
-            if(st.empty()){
-                temp[i] = -1;
+            if(!st.empty()){
+                temp[i] = st.top();
             }
-            else temp[i] = st.top();
             st.push(nums2[i]);
         }
+        for(int i = 0;i<n;i++){
+            maps[nums2[i]] = temp[i];
+        }
         vector<int>ans;
-        for(int i =0;i<nums1.size();i++){
-            int val = nums1[i];
-            int index = mp[val];
-            ans.push_back(temp[index]);
+        for(auto it : nums1){
+            ans.push_back(maps[it]);
         }
         return ans;
     }
