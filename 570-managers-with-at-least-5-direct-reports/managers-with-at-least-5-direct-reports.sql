7a115a7c2d48e09c68505fb1180b1e319787521b
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-06-05 13:34:49 +0530

    Time: 337 ms (80.50%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,5 @@
-
-select name from employee where id in
- (select managerId from employee group by managerId having 
- count(managerId)>4);
+SELECT E.name
+FROM Employee E
+JOIN Employee E1 ON E.id = E1.managerId
+GROUP BY E.id
+HAVING COUNT(E1.id) >= 5;
