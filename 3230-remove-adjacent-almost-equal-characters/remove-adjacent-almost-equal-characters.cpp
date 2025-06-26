9fae8a8e5c569feb08a63bf758aa09df6abaf43d
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-06-26 18:56:51 +0530

    Time: 8 ms (11.23%) | Memory: 9.1 MB (26.74%) - LeetSync

--- 3230-remove-adjacent-almost-equal-characters/remove-adjacent-almost-equal-characters.cpp
+++ 3230-remove-adjacent-almost-equal-characters/remove-adjacent-almost-equal-characters.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int removeAlmostEqualCharacters(string word) {
-        int count  = 0;
-        for(int i = 0;i<word.size()-1;i++){
-            if(word[i]==word[i+1] || word[i]==word[i+1]+1 || word[i]==word[i+1]-1){
-                count++;
-                i++;
+        int count =0;
+        int n = word.size();
+        int i =0;
+        while(i<n){
+            if(word[i]==word[i+1] || word[i]==word[i+1]-1 || word[i]==word[i+1]+1){
+                cout<<i<<endl;
+                 
+                count+=1;
+                i+=2;
             }
+            else i+=1;
         }
         return count;
     }
