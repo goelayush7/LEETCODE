e1792d6c22b47f26bff8d8795cb2b4b448e98c1e
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-07-08 00:01:59 +0530

    Time: 66 ms (41.24%) | Memory: 75 MB (64.96%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -1,49 +1,28 @@
-class Solution 
-{
+class Solution {
 public:
-    int maxEvents(vector<vector<int>>& events) 
-    {
-        // Step 1: Sort events by start day
-        sort(events.begin(), events.end());
-
-        // Step 2: Min-heap for end days
-        priority_queue<int, vector<int>, greater<int>> minHeap; 
-        
-        int i = 0;
-        int count = 0;
+    int maxEvents(vector<vector<int>>& events) {
         int n = events.size();
-
-        int lastDay = 0;
-        for (const auto& e : events) 
-        {
-            lastDay = max(lastDay, e[1]);
+        int maxDay = 0;
+        for (int i = 0; i < events.size(); i++) {
+            maxDay = max(maxDay, events[i][1]);
         }
-
-        for (int day = 1; day <= lastDay; day++) 
-        {
-            while (i < n && events[i][0] == day) 
-            {
-                minHeap.push(events[i][1]);
-                i++;
+        priority_queue<int, vector<int>, greater<>> pq;
+        sort(events.begin(), events.end());
+        int ans = 0;
+        for (int i = 0, j = 0; i <= maxDay; i++) {
+            while (j < n && events[j][0] <= i) {
+                pq.emplace(events[j][1]);
+                j++;
             }
-            while (!minHeap.empty() && minHeap.top() < day) 
-            {
-                minHeap.pop();
+            while (!pq.empty() && pq.top() < i) {
+                pq.pop();
             }
-
-            if (!minHeap.empty()) 
-            {
-                minHeap.pop();
-                count++;
-            }
-
-            if (i == n && minHeap.empty())
-            {
-                break;
+            if (!pq.empty()) {
+                pq.pop();
+                ans++;
             }
         }
 
-        // Step 6: Return result
-        return count;
+        return ans;
     }
 };
