fae547e041229c8c9b3a5d1963b30d41978ec2f0
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-07-07 19:41:20 +0530

    Time: 31 ms (33.30%) | Memory: 24.5 MB (17.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>&grid,vector<vector<int>>&vis,int i ,int j){
-        vis[i][j]=1;
-        int n = grid.size();
-        int m = grid[0].size();
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1}; 
-        for(int k =0;k<4;k++){
-            int nrow = drow[k]+i;
-            int ncol = dcol[k]+j;
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0){
-                dfs(grid,vis,nrow,ncol);
+    void dfs(vector<vector<char>>& grid, int i, int j,
+             vector<vector<int>>& vis) {
+                int n = grid.size();
+                int m = grid[0].size();
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vis[i][j] = 1;
+        int drow[] = {0, -1, 0, 1};
+        int dcol[] = {-1, 0, 1, 0};
+        while (!q.empty()) {
+            auto front = q.front();
+            q.pop();
+            int row = front.first;
+            int col = front.second;
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = drow[i] + row;
+                int ncol = dcol[i] + col;
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]=='1' && vis[nrow][ncol]==-1){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int count =0;
         int n = grid.size();
-        int m  = grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(vis[i][j]==0 && grid[i][j]=='1'){
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && vis[i][j] == -1) {
                     count++;
-                    dfs(grid,vis,i,j);
+                    dfs(grid, i, j, vis);
                 }
             }
         }
