a1d6068054a6909cb175d548ba76e1408ffeedd1
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-07-07 20:50:56 +0530

    Time: 152 ms (7.58%) | Memory: 330.6 MB (5.10%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n=nums.size(); 
-        vector<int>freq(n+1,0);         
-        for(auto i:queries){
-            freq[i[0]]++;
-            freq[i[1]+1]--;
+        int n = nums.size();
+        vector<int>temp(n+1,0);
+        for(auto it : queries){
+            int start = it[0];int end = it[1];
+            temp[start]-=1;
+            temp[end+1]+=1;
         }
-        int op=0;
-        for(int i=0;i<n;i++){
-            op+=freq[i];
-            if(op<nums[i])return false;
+        vector<int>prefix;
+        int sum = 0;
+        for(auto it : temp){
+            sum+=it;
+            prefix.push_back(sum);
         }
-        return true;        
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]>abs(prefix[i]))return false;
+        }
+        return true;
     }
 };
