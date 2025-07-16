6ed5e7749c5be1d89c8d9c3d2e3ef4c431412efa
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-07-16 18:45:04 +0530

    Time: 1196 ms (64.92%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,15 +1,14 @@
 # Write your MySQL query statement below
-(SELECT name AS results
-FROM MovieRating JOIN Users USING(user_id)
-GROUP BY name
-ORDER BY COUNT(*) DESC, name
-LIMIT 1)
+-- select U.name from Users U join MovieRating m on U.user_id = m.user_id 
+-- group by U.name 
 
-UNION ALL
-
-(SELECT title AS results
-FROM MovieRating JOIN Movies USING(movie_id)
-WHERE EXTRACT(YEAR_MONTH FROM created_at) = 202002
-GROUP BY title
-ORDER BY AVG(rating) DESC, title
-LIMIT 1);
+(Select u.name as results from Users u left join MovieRating m on u.user_id = m.user_id 
+group by u.name 
+order by count(m.user_id) desc ,u.name
+limit 1)
+union all 
+(Select M.title as results from Movies M left join MovieRating m1 on M.movie_id = m1.movie_id
+where created_at like "2020-02%"
+group by M.title 
+order by avg(m1.rating) desc , M.title 
+limit 1)
