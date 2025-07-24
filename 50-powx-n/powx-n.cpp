f1399b7179adb7d26d78a483739c8fb89ace737f
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-07-24 13:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (6.94%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     double isans(double x,int n){
         if(n==0)return 1;
-        double temp = isans(x,n/2);
+        double temp = pow(x,n/2);
         temp = temp*temp;
-        if(n%2==1){
+        if(n%2!=0){
             return x*temp;
         }
         else return temp;
     }
     double myPow(double x, int n) {
-        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
+        if(n==INT_MAX)return (x==1) ? 1:(x==-1)?-1:0;
+        if(n==INT_MIN)return (x==-1 || x==1) ? 1:0;
         double ans = isans(x,abs(n));
-        if(n<0)return 1.0/ans;
+        if(n<0)ans = 1.0/ans;
         return ans;
+
+        
     }
 };
