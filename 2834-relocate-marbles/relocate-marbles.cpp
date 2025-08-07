38c206ed6fc6d1a258c98db1819fcdc83ddd029b
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-08-07 23:27:24 +0530

    Time: 103 ms (70.43%) | Memory: 182.7 MB (67.20%) - LeetSync

--- 2834-relocate-marbles/relocate-marbles.cpp
+++ 2834-relocate-marbles/relocate-marbles.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
-        unordered_map<int,int>maps;
-        for(auto it : nums){
-            maps[it]++;
-        }
-        int n = moveFrom.size();
-        for(int i=0;i<n;i++){
-            if(maps.find(moveFrom[i])!=maps.end()){
-                maps.erase(moveFrom[i]);
-                maps[moveTo[i]]++;
-            }
+    vector<int> relocateMarbles(vector<int>& nums, vector<int>& mF, vector<int>& mT) {
+        unordered_set<int>st(nums.begin(),nums.end());
+        int n = mF.size();
+        for(int i = 0;i<n;i++){
+            st.erase(mF[i]);
+            st.insert(mT[i]);
         }
         vector<int>ans;
-        for(auto it : maps){
-            ans.push_back(it.first);
+        for(auto it : st){
+            ans.push_back(it);
         }
         sort(ans.begin(),ans.end());
         return ans;
