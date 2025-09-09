4e439fa5cc344f9a728e95928bf85365bdfc45cb
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-09-09 19:12:02 +0530

    Time: 353 ms (19.12%) | Memory: 165.7 MB (17.08%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& recipes,
-                                  vector<vector<string>>& ingredients,
-                                  vector<string>& supplies) {
-        // Track available ingredients and recipes
-        unordered_set<string> available(supplies.begin(), supplies.end());
-
-        // Queue to process recipe indices
-        queue<int> recipeQueue;
-        for (int idx = 0; idx < recipes.size(); ++idx) {
-            recipeQueue.push(idx);
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        map<string,int>indegree;
+        map<string,vector<string>>adj;
+        for(int i=0;i<ingredients.size();i++){
+            for(auto it : ingredients[i]){
+                adj[it].push_back(recipes[i]);
+                indegree[recipes[i]]++;
+            }
         }
-
-        vector<string> createdRecipes;
-        int lastSize = -1;
-
-        // Continue while we keep finding new recipes
-        while (static_cast<int>(available.size()) > lastSize) {
-            lastSize = available.size();
-            int queueSize = recipeQueue.size();
-
-            // Process all recipes in current queue
-            while (queueSize-- > 0) {
-                int recipeIdx = recipeQueue.front();
-                recipeQueue.pop();
-                bool canCreate = true;
-
-                // Check if all ingredients are available
-                for (string& ingredient : ingredients[recipeIdx]) {
-                    if (!available.count(ingredient)) {
-                        canCreate = false;
-                        break;
-                    }
-                }
-
-                if (!canCreate) {
-                    recipeQueue.push(recipeIdx);
-                } else {
-                    // Recipe can be created - add to available items
-                    available.insert(recipes[recipeIdx]);
-                    createdRecipes.push_back(recipes[recipeIdx]);
+        queue<string>q;
+        for(auto it : supplies){
+            if(indegree[it]==0){
+                q.push(it);
+            }
+        }
+        vector<string>ans;
+        while(!q.empty()){
+            auto front = q.front();
+            q.pop();
+            for(auto it : adj[front]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    ans.push_back(it);
+                    q.push(it);
                 }
             }
         }
-
-        return createdRecipes;
+        return ans;
     }
 };
