581ce081a19001afb3e0f6f799d700dd7ae47f58
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-09-19 14:56:59 +0530

    Time: 450 ms (8.18%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,10 @@
-# Write your MySQL query statement below
-select
-(select distinct Salary 
-from Employee order by salary desc 
-limit 1 offset 1) 
-as SecondHighestSalary;
+WITH CTE AS (
+    SELECT 
+        ID,
+        SALARY,
+        DENSE_RANK() OVER(ORDER BY SALARY DESC) AS RNK
+    FROM EMPLOYEE
+)
+SELECT IFNULL((
+    SELECT SALARY FROM CTE WHERE RNK = 2 LIMIT 1
+),NULL) AS SecondHighestSalary
