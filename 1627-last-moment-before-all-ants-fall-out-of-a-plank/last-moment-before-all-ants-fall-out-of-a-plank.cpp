2e50f7716f9ccb9aa08fc8ea35a0ce900ceab13c
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2025-09-22 19:01:21 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (99.55%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         int ans = 0;
-        for(auto it : left){
-            ans = max(ans,abs(0-it));
+        for(int i = 0 ; i <left.size();i++){
+            ans = max(ans,abs(0-left[i]));
         }
-        for(auto it : right){
+        for(auto it :right){
             ans = max(ans,abs(n-it));
         }
         return ans;
